3b6ca250b91dbe054550f2ceb90d3f05175cca29
Tuwebai <tuwebai@gmail.com>
2026-05-21 21:32:37 -0300

    Bump app version to 1.0.113

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.juan.snapmusic"
         minSdk = 24
         targetSdk = 34
-        versionCode = 112
-        versionName = "1.0.112"
+        versionCode = 113
+        versionName = "1.0.113"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
